67000fa8a2d4719e4b629895f132eb2bcecc4220
Adrienne <adrie.k@gmail.com>
2013-10-14 00:27:15 -0700

    No more raw_input()

--- dominion/run.py
+++ dominion/run.py
@@ -4,11 +4,11 @@ from dominiongirl import give10
 
 app = Flask(__name__)
 
-#@app.route("/")
-#def hello():
+@app.route("/")
+def hello():
     # return "Hello World!"
-#    print ' '.join(give10('Seaside'))
-#    return ' '.join(give10('Seaside'))
+    print ' '.join(give10('seaside'))
+    return ' '.join(give10('seaside'))
 
 if __name__ == "__main__":
     app.debug = True
