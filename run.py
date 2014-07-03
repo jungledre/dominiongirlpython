495302224bd9281f6e4d4a3b2c44c9919a67a5a4
Adrienne <adrie.k@gmail.com>
2014-07-03 13:39:24 -0700

    Port change.

--- run.py
+++ run.py
@@ -27,4 +27,5 @@ def bower_components(filename):
     return send_from_directory(os.path.join(app.root_path, 'bower_components'), filename)
 
 if __name__ == "__main__":
-    app.run(debug = True, port=33507, host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", debug=True, port=port)
